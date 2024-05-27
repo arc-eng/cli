ee0ee54f422d825e4ed297df735198ad8bdc8ba2
Marco Lamina <mlamina09@gmail.com>
2024-05-27 16:30:45 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pr-pilot-cli',
-    version='1.0.0',
+    version='1.0.3',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
