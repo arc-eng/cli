bb04da592cc600823008dbc68b24bf18081fc2b4
Marco Lamina <mlamina09@gmail.com>
2024-05-27 15:58:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,13 @@ setup(
     install_requires=[
         line.strip() for line in open('requirements.txt').readlines()
     ],
+    python_requires='>=3.6',
+    author='Marco Lamina',
+    author_email='marco@pr-pilot.ai',
+    description='CLI for PR Pilot, a text-to-task automation platform for Github.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/PR-Pilot-AI/pr-pilot-cli',
     entry_points='''
         [console_scripts]
         pilot=cli:main
