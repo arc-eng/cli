1648f69dbfd4b120a6beac1832177c0805f4bd21
Marco Lamina <mlamina09@gmail.com>
2024-06-02 21:45:12 -0700

    Bump version to 1.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pr-pilot-cli',
-    version='1.6.0',
+    version='1.6.1',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
