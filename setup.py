04fd26138370710c4d9ddf0632fd5b4be876d4a8
Marco Lamina <mlamina09@gmail.com>
2024-06-13 12:15:33 -0700

    Bump version to 1.8.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pr-pilot-cli',
-    version='1.8.4',
+    version='1.8.5',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
