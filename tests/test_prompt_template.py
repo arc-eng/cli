df6d8f178890ffa4fed75486e7d10baa6f7ec4f6
Marco Lamina <mlamina09@gmail.com>
2024-06-26 18:17:56 -0700

    Add unit tests for PrompTemplate

--- tests/test_prompt_template.py
+++ tests/test_prompt_template.py
@@ -1,7 +1,7 @@
-import subprocess
-from unittest.mock import Mock, patch
-import tempfile
 import os
+import subprocess
+import tempfile
+from unittest.mock import Mock, patch
 
 from cli.prompt_template import sh, PromptTemplate
 
@@ -45,13 +45,17 @@ def test_shell_command_execution_with_list_input(mock_subprocess_run):
     status.stop.assert_called_once()
 
 
-def test_prompt_template_render():
+@patch("cli.prompt_template.is_git_repo")
+@patch("cli.prompt_template.get_git_root")
+def test_prompt_template_render(mock_is_git_repo, mock_get_git_root):
     status = Mock()
     template_content = "Hello, {{ name }}!"
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        template_file_path = os.path.join(tmpdirname, "test_template.jinja")
-        with open(template_file_path, "w") as f:
+        os.mkdir(os.path.join(tmpdirname, "test_repo"))
+        template_file_path = "test_repo/test_model.jinja2"
+
+        with open(os.path.join(tmpdirname, template_file_path), "w") as f:
             f.write(template_content)
 
         prompt_template = PromptTemplate(
@@ -59,8 +63,116 @@ def test_prompt_template_render():
             repo="test_repo",
             model="test_model",
             status=status,
-            name="World"
+            name="World",
+            home=tmpdirname,
         )
 
         result = prompt_template.render()
         assert result == "Hello, World!"
+        mock_is_git_repo.assert_not_called()
+        mock_get_git_root.assert_not_called()
+
+
+@patch("cli.prompt_template.is_git_repo")
+@patch("cli.prompt_template.get_git_root")
+def test_determine_template_home(mock_get_git_root, mock_is_git_repo):
+    status = Mock()
+    mock_is_git_repo.return_value = True
+    mock_get_git_root.return_value = "/path/to/git/root"
+
+    prompt_template = PromptTemplate(
+        template_file_path="test_model.jinja2",
+        repo="test_repo",
+        model="test_model",
+        status=status,
+        name="World",
+    )
+
+    assert prompt_template.home == "/path/to/git/root"
+    mock_is_git_repo.assert_called_once()
+    mock_get_git_root.assert_called_once()
+
+
+@patch("cli.prompt_template.is_git_repo")
+@patch("cli.prompt_template.get_git_root")
+def test_determine_template_home_no_git_repo(mock_get_git_root, mock_is_git_repo):
+    status = Mock()
+    mock_is_git_repo.return_value = False
+
+    prompt_template = PromptTemplate(
+        template_file_path="test_model.jinja2",
+        repo="test_repo",
+        model="test_model",
+        status=status,
+        name="World",
+    )
+
+    assert prompt_template.home == os.getcwd()
+    mock_is_git_repo.assert_called_once()
+    mock_get_git_root.assert_not_called()
+
+
+@patch("cli.prompt_template.is_git_repo")
+@patch("cli.prompt_template.get_git_root")
+@patch("cli.prompt_template.os.getcwd")
+def test_get_template_file_path__relative_to_cwd(mock_getcwd, mock_get_git_root, mock_is_git_repo):
+    # Create temporary directory as "git repo"
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        status = Mock()
+        mock_is_git_repo.return_value = True
+        mock_get_git_root.return_value = tmpdirname
+
+        # Create a sub-directory and a file
+        os.mkdir(os.path.join(tmpdirname, "test_repo"))
+        template_file_path = "test_repo/test_model.jinja2"
+        with open(os.path.join(tmpdirname, template_file_path), "w") as f:
+            f.write("Hello, {{ name }}!")
+
+        # Simulate "pilot run" command in sub-directory of the repo
+        mock_getcwd.return_value = os.path.join(tmpdirname, "test_repo")
+        prompt_template = PromptTemplate(
+            template_file_path="test_model.jinja2",
+            repo="test_repo",
+            model="test_model",
+            status=status,
+            name="World",
+        )
+
+        # Should return the template path relative to the git root
+        assert prompt_template.get_template_file_path() == "test_repo/test_model.jinja2"
+        mock_is_git_repo.assert_called_once()
+        mock_get_git_root.assert_called_once()
+
+
+@patch("cli.prompt_template.is_git_repo")
+@patch("cli.prompt_template.get_git_root")
+@patch("cli.prompt_template.os.getcwd")
+def test_get_template_file_path__relative_to_git_dir(
+    mock_getcwd, mock_get_git_root, mock_is_git_repo
+):
+    # Create temporary directory as "git repo"
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        status = Mock()
+        mock_is_git_repo.return_value = True
+        mock_get_git_root.return_value = tmpdirname
+
+        # Create a sub-directory and a file
+        os.mkdir(os.path.join(tmpdirname, "test_repo"))
+        template_file_path = "test_repo/test_model.jinja2"
+        with open(os.path.join(tmpdirname, template_file_path), "w") as f:
+            f.write("Hello, {{ name }}!")
+
+        # Simulate "pilot run" command in root of the repo
+        mock_getcwd.return_value = tmpdirname
+        prompt_template = PromptTemplate(
+            template_file_path="test_repo/test_model.jinja2",
+            repo="test_repo",
+            model="test_model",
+            status=status,
+            name="World",
+        )
+
+        # Should return the template path relative to the git root
+        assert prompt_template.get_template_file_path() == "test_repo/test_model.jinja2"
+        mock_is_git_repo.assert_called_once()
+        mock_get_git_root.assert_called_once()
