89c24f4d5d698f04bb368107ebccfdad8bf12bc0
Marco Lamina <mlamina09@gmail.com>
2024-10-30 18:30:18 -0700

    arcane-engine-cli > arcane-cli

--- cli/commands/upgrade.py
+++ cli/commands/upgrade.py
@@ -7,21 +7,21 @@ from rich.prompt import Confirm
 
 @click.command()
 def upgrade():
-    """⬆️ Upgrade arcane-engine-cli to the latest version."""
+    """⬆️ Upgrade arcane-cli to the latest version."""
     if is_installed_via_homebrew():
         if Confirm.ask(
-            "Found homebrew installation. Upgrade the [code]arcane-engine-cli[/code] package?"
+            "Found homebrew installation. Upgrade the [code]arcane-cli[/code] package?"
         ):
             subprocess.run(["brew", "update"], check=True)
-            subprocess.run(["brew", "upgrade", "arcane-engine-cli"], check=True)
+            subprocess.run(["brew", "upgrade", "arcane-cli"], check=True)
     else:
-        if Confirm.ask("Upgrade the [code]arcane-engine-cli[/code] package with pip?"):
+        if Confirm.ask("Upgrade the [code]arcane-cli[/code] package with pip?"):
             subprocess.run(
-                [sys.executable, "-m", "pip", "install", "--upgrade", "arcane-engine-cli"], check=True
+                [sys.executable, "-m", "pip", "install", "--upgrade", "arcane-cli"], check=True
             )
 
 
 def is_installed_via_homebrew() -> bool:
-    """Check if arcane-engine-cli is installed via Homebrew."""
-    result = subprocess.run(["brew", "list", "arcane-engine-cli"], capture_output=True)
+    """Check if arcane-cli is installed via Homebrew."""
+    result = subprocess.run(["brew", "list", "arcane-cli"], capture_output=True)
     return result.returncode == 0
